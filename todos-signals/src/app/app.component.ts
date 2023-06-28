66385c723706af0a630e8c3a50fd7537fedea086
Alain D'Ettorre <alain.det@gmail.com>
2023-06-28 17:44:52 +0200

    Remove module to make todos app standalone

--- todos-signals/src/app/app.component.ts
+++ todos-signals/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
